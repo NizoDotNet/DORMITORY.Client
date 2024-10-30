1e2982099c68791418cae41c58957e029ebbe7f3
NizoDotNet <mmmdovnizami.official@gmail.com>
2024-10-30 18:06:04 +0400

    last commit before refactoring

--- src/router/router.js
+++ src/router/router.js
@@ -5,12 +5,14 @@ import HomeView from '@/views/HomeView.vue'
 import UserView from "@/views/UserView.vue";
 import RegisterView from "@/views/RegisterView.vue"
 import UserIdView from "@/views/UserIdView.vue";
+import ServerErrorView from "@/views/ServerErrorView.vue";
 const routes = [
     { path: '/', component: HomeView },
     { path: '/login', component: LoginView },
     { path: '/user', component: UserView },
     { path: '/users/:id', component: UserIdView },
     { path: '/register', component: RegisterView },
+    { path: '/server-error', component: ServerErrorView}
 ]
 
 export default createRouter({
---
+++ src/views/ServerErrorView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<h2 class="text-center">500</h2>
+</template>
+
+<style scoped>
+
+</style>
