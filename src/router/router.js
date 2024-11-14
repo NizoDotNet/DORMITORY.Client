62678db9760f63a69dc67c55e2e4acce729f92cd
NizoDotNet <mmmdovnizami.official@gmail.com>
2024-11-14 23:32:02 +0400

    contact

--- src/router/router.js
+++ src/router/router.js
@@ -6,13 +6,15 @@ import UserView from "@/views/UserView.vue";
 import RegisterView from "@/views/RegisterView.vue"
 import UserIdView from "@/views/UserIdView.vue";
 import ServerErrorView from "@/views/ServerErrorView.vue";
+import ContactView from "@/views/ContactView.vue";
 const routes = [
     { path: '/', component: HomeView },
     { path: '/login', component: LoginView },
     { path: '/user', component: UserView },
     { path: '/users/:id', component: UserIdView },
     { path: '/register', component: RegisterView },
-    { path: '/server-error', component: ServerErrorView}
+    { path: '/server-error', component: ServerErrorView},
+    { path: "/contact", component: ContactView}
 ]
 
 export default createRouter({
---
+++ src/views/ContactView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+Elaqe nomresi: 1000032
+</template>
+
+<style scoped>
+
+</style>
