a160c7e007a4f3cd2eb9faa74fd19eccf9c2c477
NizoDotNet <mmmdovnizami.official@gmail.com>
2024-12-12 20:08:15 +0400

    jwt to localstorage

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -22,6 +22,7 @@ const login = async () => {
       password: loginData.password,
     });
     if (res.status === 200) {
+      localStorage.setItem('jwt', res.data);
       await store.getUser();
       await router.push("/user");
     }
