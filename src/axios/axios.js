653a74530b7aac0835753cf5964dc7d04331f66b
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-05-28 23:38:31 +0400

    now it works on phone too

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost";
+// axios.defaults.baseURL = "http://localhost";
 // axios.defaults.headers['Authorization'] = 'Bearer ' + localStorage.getItem('jwt')
