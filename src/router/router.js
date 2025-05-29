b85ee9103271c2274bf3c7424c500d6baa604d84
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-05-29 13:02:47 +0400

    need rewrite uri

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,13 @@
-import {createMemoryHistory, createRouter, createWebHistory} from 'vue-router'
+import {
+  createWebHashHistory,
+  createRouter,
+  createWebHistory,
+} from "vue-router";
 
-import LoginView from '@/views/LoginView.vue'
-import HomeView from '@/views/HomeView.vue'
+import LoginView from "@/views/LoginView.vue";
+import HomeView from "@/views/HomeView.vue";
 import UserView from "@/views/UserView.vue";
-import RegisterView from "@/views/RegisterView.vue"
+import RegisterView from "@/views/RegisterView.vue";
 import UserIdView from "@/views/UserIdView.vue";
 import ServerErrorView from "@/views/ServerErrorView.vue";
 import ContactView from "@/views/ContactView.vue";
@@ -11,20 +15,19 @@ import UsersView from "@/views/UsersView.vue";
 import UserIdViewFromQr from "@/views/UserIdViewFromQr.vue";
 import UpdateUserView from "@/views/UpdateUserView.vue";
 const routes = [
-    { path: '/', component: HomeView },
-    { path: '/login', component: LoginView },
-    { path: '/user', component: UserView },
-    { path: "/user/update", component: UpdateUserView},
-    { path: '/users/:id', component: UserIdView, name: 'User' },
-    { path: '/users/qr/:id', component: UserIdViewFromQr},
-    { path: '/register', component: RegisterView },
-    { path: '/server-error', component: ServerErrorView},
-    { path: "/contact", component: ContactView},
-    { path: "/users", component: UsersView}
-]
+  { path: "/", component: HomeView },
+  { path: "/login", component: LoginView },
+  { path: "/user", component: UserView },
+  { path: "/user/update", component: UpdateUserView },
+  { path: "/users/:id", component: UserIdView, name: "User" },
+  { path: "/users/qr/:id", component: UserIdViewFromQr },
+  { path: "/register", component: RegisterView },
+  { path: "/server-error", component: ServerErrorView },
+  { path: "/contact", component: ContactView },
+  { path: "/users", component: UsersView },
+];
 
 export default createRouter({
-    history: createWebHistory(),
-    routes,
-})
-
+  history: createWebHashHistory(),
+  routes,
+});
