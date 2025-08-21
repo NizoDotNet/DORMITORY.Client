f948fbe905959be7d3ec2dc9b131899cc657874e
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-08-21 18:14:49 +0400

    white p in reprimands and history

--- src/components/HistoryComponent.vue
+++ src/components/HistoryComponent.vue
@@ -1,35 +1,44 @@
 <script setup>
-import moment from 'moment';
-import {onMounted, ref} from "vue";
+import moment from "moment";
+import { onMounted, ref } from "vue";
 import axios from "axios";
 const props = defineProps({
-  userId: Text
-})
-const histories = ref([])
+  userId: Text,
+});
+const histories = ref([]);
 let pageNumber = 1;
 onMounted(async () => {
   try {
     const res = await getHistories(props.userId, pageNumber);
-    if(res.status === 200) {
-      histories.value = res.data
+    if (res.status === 200) {
+      histories.value = res.data;
     }
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-})
+});
 
 async function getHistories(userId, pageNumber) {
-  return await axios.get(`/api/users/${userId}/histories?page=${pageNumber}`)
+  return await axios.get(`/api/users/${userId}/histories?page=${pageNumber}`);
 }
 </script>
 
 <template>
   <div class="histories" v-for="history in histories" :key="history.id">
     <hr align="center" color="#dddddd" />
-    <p>Tarix: {{ history.dateTime.substring(11, 16) + " " +  moment(history.dateTime.substring(0, 10)).format("DD-MM-YYYY")}}</p>
+    <p>
+      Tarix:
+      {{
+        history.dateTime.substring(11, 16) +
+        " " +
+        moment(history.dateTime.substring(0, 10)).format("DD-MM-YYYY")
+      }}
+    </p>
   </div>
 </template>
 
 <style scoped>
-
+p {
+  color: white;
+}
 </style>
--- src/components/ReprimandComponent.vue
+++ src/components/ReprimandComponent.vue
@@ -1,15 +1,15 @@
 <script setup>
 import moment from "moment";
 defineProps({
-  reprimands: Array
-})
+  reprimands: Array,
+});
 </script>
 
 <template>
-<!--  <div class="d-flex justify-content-center">
+  <!--  <div class="d-flex justify-content-center">
     <h5 class="text-danger" v-if="reprimands.length > 0">Töhmətlər</h5>
   </div>-->
-  <div v-for="reprimand in reprimands" class="row">
+  <div v-for="reprimand in reprimands" :key="reprimand.id" class="row">
     <hr align="center" color="#dddddd" />
     <div class="text-break">
       <p>TÖHMƏTİN AÇIQLAMASI: {{ reprimand.text }}</p>
@@ -17,11 +17,12 @@ defineProps({
     <div class="text-break">
       <p>ƏMRİN NÖMRƏSİ: {{ reprimand.code }}</p>
     </div>
-    <p>Tarix: {{ moment(reprimand.dateTime.substring(0, 10)).format("DD-MM-YYYY") }}</p>
+    <p>
+      Tarix:
+      {{ moment(reprimand.dateTime.substring(0, 10)).format("DD-MM-YYYY") }}
+    </p>
     <p>Tip: {{ reprimand.type }}</p>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
