a6b7588cefeb976984ed9cc8586b8de087d579ff
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-08-29 12:39:31 +0400

    1 course

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -308,7 +308,7 @@ watch(conflict, () => {
                 id=""
                 class="form-select"
               >
-                <!-- <option :value="1">1</option> -->
+                <option :value="1">1</option>
                 <option :value="2">2</option>
                 <option :value="3">3</option>
                 <option :value="4">4</option>
