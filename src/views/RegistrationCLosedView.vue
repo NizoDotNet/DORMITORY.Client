27d80f7856b0deb14745430d36f86b63e8257cb5
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-09-02 11:54:43 +0400

    minor changes

--- src/views/RegistrationCLosedView.vue
+++ src/views/RegistrationCLosedView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="d-flex justify-content-center p-5 text-center">
-    Saat 14:00-dan sonra qeydiyyat sadəcə 1-ci kurs tələbələri üçün aktiv
-    olacaq(əgər qəbz və digər sənədləri yükləmək istəyirsinizsə 14:00-dan sonra
-    sayta daxil ola bilərsiniz).
+    <h4 class="h-1">
+      Saat 14:00-dan sonra qeydiyyat sadəcə 1-ci kurs tələbələri üçün aktiv
+      olacaq(əgər qəbz və digər sənədləri yükləmək istəyirsinizsə 14:00-dan
+      sonra sayta daxil ola bilərsiniz).
+    </h4>
   </div>
 </template>
