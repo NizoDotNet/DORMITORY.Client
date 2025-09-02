0059cc2b2e8d16f3c12f2ffe85e0e9c0057dcdc0
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-09-02 11:49:57 +0400

    registration closed view

---
+++ src/views/RegistrationCLosedView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="d-flex justify-content-center p-5 text-center">
+    Saat 14:00-dan sonra qeydiyyat sadəcə 1-ci kurs tələbələri üçün aktiv
+    olacaq(əgər qəbz və digər sənədləri yükləmək istəyirsinizsə 14:00-dan sonra
+    sayta daxil ola bilərsiniz).
+  </div>
+</template>
